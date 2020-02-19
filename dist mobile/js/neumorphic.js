582aea28558b98ce31bcd418a6b9001ccf99f819
Kevin Schrader <kevin.schrader@student.ehb.be>
2020-02-19 16:58:55 +0100

    new version

--- dist mobile/js/neumorphic.js
+++ dist mobile/js/neumorphic.js
@@ -50,18 +50,15 @@ $(document).ready(function () {
     $("#plus_btn").click(function () {
         $("#guest").show();
         $("#personal_info").hide();
-        $("#plus_btn").hide();
     });
     $("#close").click(function () {
         $("#guest").hide();
         $("#personal_info").show();
-        $("#plus_btn").show();
     });
     $("#save").click(function () {
         $("#guest").hide();
         $('#guest_list').show();
         $("#personal_info").show();
-        $("#plus_btn").show();
 
         let str_guest_email = "";
         let str_guest_name = "";
@@ -76,8 +73,6 @@ $(document).ready(function () {
         $("#guest_email_list").text("");
         $('#guest_list').hide();
     });
-
-    //get data from guest and inserting in guest list
 
     //neumorphic elements
     $('.neumorphic-checkbox').on('click', function () {
